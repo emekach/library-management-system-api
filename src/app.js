6fe400c39352def2fdfa37c563009a4352dcbffa
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-13 19:51:47 +0000

    feat: implement rate limiter

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const limiter = require('./utils/rateLimit');
+
 const app = express();
+
+// cors
+app.use(
+  cors({
+    origin: process.env.ALLOWED_URI,
+    credentials: true,
+  })
+);
+
+// security (Set http security header)
+app.use(helmet());
+
+app.use(morgan('dev'));
+app.use(limiter);
 
 app.use(express.json({ limit: '15kb' }));
 app.use(express.urlencoded({ extended: true, limit: '15kb' }));
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP' });
+});
 
 module.exports = app;
