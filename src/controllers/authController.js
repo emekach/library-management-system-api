f6b9a2366760c1bae713478b68ceaee3f0ed8e74
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-15 00:00:43 +0000

    feat: implement register user controller and router

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,1 +1,32 @@
-exports.createUser = (req, res) => {};
+const AppError = require('./../utils/AppError');
+const catchAsync = require('./../utils/catchAsync');
+const User = require('./../models/userModel');
+
+exports.createUser = catchAsync(async (req, res, next) => {
+  const { name, email, password, passwordConfirm } = req.body;
+
+  const emailExist = await User.findOne({ email });
+  if (emailExist) {
+    return next(new AppError('User with this Email Already Exist', 400));
+  }
+
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+  });
+
+  const createdUser = await User.findById(newUser._id).select('-password');
+
+  if (!createdUser) {
+    return next(new AppError('Failed to create User', 400));
+  }
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: createdUser,
+    },
+  });
+});
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,6 +3,6 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.route('/auth').post(authController.createUser);
+router.route('/auth/register').post(authController.createUser);
 
 module.exports = router;
