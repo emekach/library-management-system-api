0bfae1de12669613afd782a2ac95e045c0a73319
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-17 21:21:04 +0000

    feat: implement permission middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -38,3 +38,17 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError(
+          'you do not have the permission to perform this action',
+          403
+        )
+      );
+    }
+    next();
+  };
+};
